fcbbf2ef416fba24720e4eaefd0c2dace9dfe728
Gogul24 <158571417+Gogul24@users.noreply.github.com>
2024-03-21 10:33:56 +0530

    java tasks

---
+++ CircularLinkedList/CllMain.java
@@ -0,0 +1,14 @@
+package CircularLinkedList;
+
+public class CllMain {
+
+	public static void main(String[] args) {
+		
+		CircularLinkedListPerson circularLinkedListPerson = new CircularLinkedListPerson();
+		circularLinkedListPerson.insert("Gokul");
+		circularLinkedListPerson.insert("RTO");
+		circularLinkedListPerson.insert("CM");
+		circularLinkedListPerson.print();
+	}
+
+}
---
+++ CircularLinkedList/CllPerson.java
@@ -0,0 +1,10 @@
+package CircularLinkedList;
+
+public class CllPerson {
+	
+	String name;
+	CllPerson connectNext=null;
+	public CllPerson(String name) {
+		this.name=name;
+	}
+}
